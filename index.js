59aa9adbef33f0c4414d5c813a65adff97fbca9a
Dave Schmidt <daveschmidt@sridev.com>
2019-02-06 00:10:41 -0800

    improved logger and separated out into middleware folder

--- index.js
+++ index.js
@@ -1,31 +1,10 @@
 let express = require('express');
+let middleware = require('./lib/middleware');
 let routes = require('./lib/routes');
 
 let app = express();
 
-app.use(function (req, res, next) {
-    let startDateTime = new Date();
-    res.on('finish', function() {
-        let endDateTime = new Date();
-        console.info({
-          data  : {
-            duration: `${endDateTime - startDateTime} ms`,
-            req: {
-                method: req.method,
-                url   : req.originaUrl,
-                ip    : req.ip,
-                dateTime: startDateTime
-              },
-            res: {
-                dateTime: endDateTime,
-                status_code: res.statusCode
-              }
-          }
-        });
-    });
-
-    next();
-});
+app.use(middleware.logger);
 
 app.use(routes.personRoute);
 
@@ -33,4 +12,5 @@ app.use((req,res,next) => {
   res.status(404).send('Resource Not Found')
 });
 
-app.listen(3000, () => console.info('server started...'))
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.info(`server started, listening on port ${PORT}`))
